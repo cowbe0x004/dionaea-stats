5cdfb31ff6f57b39a26455a3eb155a3b8edebcb1
root <root@pot3.nospaceindent.com>
2015-03-25 09:09:16 -0400

    added export to text

--- show_stats.py
+++ show_stats.py
@@ -5,13 +5,18 @@ from prettytable import PrettyTable
 import sqlite3
 import sys
 
+# opening sqlite db and cursor
 db_file = '/var/dionaea/logsql.sqlite'
-
 conn = sqlite3.connect(db_file)
 cursor = conn.cursor()
 
+# saving output to file
+output = open('export.txt', 'w')
+# empty content from previous run
+output.truncate()
+
 # printing the data
-def printTable(cursor):
+def printTable(cursor, sqlTitle):
     # get column names from description
     col_names = [cn[0] for cn in cursor.description]
     rows = cursor.fetchall()
@@ -28,9 +33,17 @@ def printTable(cursor):
         x = x + 1
     
 #    y.align[col_names[1]] = "r"
+    print sqlTitle
     print(y)
     print
-    
+    outputData = y.get_string()
+
+    # saving output to file
+    output = open('export.txt', 'a')
+    output.write(sqlTitle + "\n")
+    output.write(outputData + "\n\n")
+    output.close()
+
 # printing column names
 #def printCol(cursor):
 #    col_names = [cn[0] for cn in cursor.description]
@@ -44,7 +57,7 @@ def printTable(cursor):
 #        print '%-10d %s' % (col[0], col[1])
 
 # attacked ports
-print "Attacked ports"
+sqlTitle = "Attacked ports"
 cursor.execute("""SELECT COUNT(local_port) AS hitcount, local_port AS port 
     FROM connections 
     WHERE connection_type = 'accept' 
@@ -52,10 +65,10 @@ cursor.execute("""SELECT COUNT(local_port) AS hitcount, local_port AS port
     HAVING count(local_port) > 10
     ORDER BY COUNT(local_port) DESC
     """)
-printTable(cursor)
+printTable(cursor, sqlTitle)
 
 # latest attackers
-print "Latest Attackers"
+sqlTitle = "Latest Attackers"
 cursor.execute("""
     SELECT connection_protocol, datetime(connection_timestamp, 'unixepoch') as time, local_port, remote_host
     FROM connections
@@ -64,30 +77,30 @@ cursor.execute("""
     ORDER BY time DESC
     LIMIT 5
     """)
-printTable(cursor)
+printTable(cursor, sqlTitle)
 
 # attacks over a day
-print "Attacks over a day"
+sqlTitle = "Attacks over a day"
 cursor.execute("""
     SELECT ROUND((connection_timestamp % (3600*24)) / 3600) AS hour, COUNT(*) AS hits
     FROM connections
     WHERE connection_parent IS NULL
     GROUP BY ROUND((connection_timestamp % (3600*24)) / 3600)
     """)
-printTable(cursor)
+printTable(cursor, sqlTitle)
 
 # popular malware downloads
-print "Popular malware downloads"
+sqlTitle = "Popular malware downloads"
 cursor.execute("""
     SELECT COUNT(download_md5_hash), download_md5_hash 
     FROM downloads 
     GROUP BY download_md5_hash 
     ORDER BY COUNT(download_md5_hash) DESC
     """)
-printTable(cursor)
+printTable(cursor, sqlTitle)
 
 # most attacks from remote hosts
-print "Most attacks from remote hosts"
+sqlTitle = "Most attacks from remote hosts"
 cursor.execute("""
     SELECT COUNT(remote_host), remote_host
     FROM connections
@@ -96,16 +109,17 @@ cursor.execute("""
     ORDER BY count(remote_host) DESC
     LIMIT 10
     """)
-printTable(cursor)
+printTable(cursor, sqlTitle)
 
 # popular download locations
-print "Popular download locations"
+sqlTitle = "Popular download locations"
 cursor.execute("""
     SELECT COUNT(*) AS hits, download_url
     FROM downloads
     GROUP BY download_url
     ORDER BY COUNT(*) DESC
     """)
-printTable(cursor)
+printTable(cursor, sqlTitle)
 
+output.close()
 conn.close()
